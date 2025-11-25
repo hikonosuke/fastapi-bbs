1f6dad54cc452f7e980e58bc6e404e0a3208d498
ban <ban@ckg.ac.jp>
2025-11-25 09:08:35 +0900

    feature: 投稿(Post)APIとスキーマの追加

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routers import threads
+from app.routers import posts
 
 app = FastAPI(
     title="BBS API",
@@ -7,5 +8,7 @@ app = FastAPI(
     version="0.1"
 )
 
-# ルーターを登録（まだ中身は空でOK）
+
 app.include_router(threads.router)
+app.include_router(posts.router)
+app.include_router(posts.threads_router)
